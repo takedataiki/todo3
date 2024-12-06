f3f2071a81e33b5eec3d2c47744e013ff1c5269f
Takeda Taiki <take5.tai412@gmail.com>
2024-12-07 01:28:08 +0900

    Update todo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="container">
+  <div class="container p-3">
     <div class="input-group mb-5">
       <input
         v-model="newTodo"
         @keyup.enter="addTodo"
+        placeholder="新しいTodoを入力"
         class="form-control"
       >
       <button 
@@ -40,15 +41,26 @@ export default {
       todos: []
     }
   },
+  created() {
+    const savedTodos = localStorage.getItem('todos')
+    if (savedTodos) {
+      this.todos = JSON.parse(savedTodos)
+    }
+  },
   methods: {
     addTodo() {
       if (this.newTodo.trim()) {
         this.todos.push({ name: this.newTodo })
       this.newTodo = ''
+      this.saveTodos()
       }
     },
     removeTodo(index) {
       this.todos.splice(index, 1)
+      this.saveTodos()
+    },
+    saveTodos() {
+      localStorage.setItem('todos', JSON.stringify(this.todos))
     }
   }
 }
